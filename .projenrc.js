8c4941fc0166f2a63afd99d61d18182f3ef301a3
Eli Polonsky <epolon@amazon.com>
2023-02-28 19:44:12 +0200

    chore(deps:) upgrade dependencies (#5)
    Co-authored-by: Polonsky <epolon@a483e7b9e4e1.ant.amazon.com>

---
+++ .projenrc.js
@@ -0,0 +1,21 @@
+const { Cdk8sTeamNodeProject } = require('@cdk8s/projen-common');
+
+const project = new Cdk8sTeamNodeProject({
+  name: 'root',
+  repoName: 'cdk8s-ack-image-resizer',
+  release: false,
+  defaultReleaseBranch: 'main',
+  devDeps: [
+    'lerna',
+    '@cdk8s/projen-common',
+  ]
+});
+
+project.package.addField('private', true);
+project.package.addField('workspaces', {
+  packages: ['packages/**']
+});
+
+project.compileTask.exec('lerna run build');
+
+project.synth();
